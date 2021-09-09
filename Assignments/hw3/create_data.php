b100399e87141257e81e57c35941b12e9078c5a7
Derek <55250148+DLB832@users.noreply.github.com>
2021-09-08 21:42:07 -0400

    Add files via upload
    TODO: generate an html table with unique values for first name, last name, street address, and email address.
    TODO: create an output file for the personal data created and displayed in the html table with format firstName:lastName:streetAdress:emailAddress \n

--- Assignments/hw3/create_data.php
+++ Assignments/hw3/create_data.php
@@ -5,16 +5,17 @@
     Purpose: read the text files and process the data into arrays to be output into an html and text file.-->
 
 <html>
-    <!--TODO: read data from text files into arrays
+    <!--PHP code should:
+        read data from text files into arrays
             text files:
                 domains.txt -- TODO: need to combine domain name and com to produce full domain name. ex: "hotmail.com"
                 first_names.csv (Comma Seperated Values)
                 last_names.txt
                 street_names.txt
-                street_types.txt-->
-    <!--TODO: use print_r function to debug print and display the data on screen with a heading above each array-->
-    <!--TODO: generate am html table of unique customer information for 25 people-->
-    <!--TODO: write customer data to a file "customers.txt" formatted as: firstName:lastName:address:email \n-->
+                street_types.txt
+        use print_r function to debug print and display the data on screen with a heading above each array
+        generate am html table of unique customer information for 25 people
+        write customer data to a file "customers.txt" formatted as: firstName:lastName:address:email \n-->
     <head>
         <title>Create Data</title>
     </head>
@@ -27,15 +28,14 @@
             fclose($domainsFile); //closes the file for the OS.
             
             $domains = str_replace(array("\n", "\r"), '', $domains); //removes the new line characters from the string and casts it into an array
-
             $domainsArray = explode(".", $domains); //explode() removes the specified delimiter 
-            //TODO:needs to create an additional array and combine the domain type and domain ending
+
             $domainsCount = sizeof($domainsArray); //hold the size of the array
             $combinedDomainsArray = array(); //create a new array for concatenating the domain name and type together
             for ($i=0; $i < $domainsCount - 1; $i++) { 
                 $j = $i + 1; //for storing the domain type
                 $combinedDomainsArray[$i] = "$domainsArray[$i]" .".". "$domainsArray[$j]"; //new array is a concatenation of the domain, dot, domain type
-                $i++;
+                $i++; //we want to incriment twice
             }
 
             print("<pre>");
@@ -43,6 +43,7 @@
             //print_r($domainsArray); //for debugging
             print_r($combinedDomainsArray);
             print("</pre>");
+            
 
             //First Names
             $firstNameFile =fopen("first_names.csv","r"); //arguments specify the file, the permissions (read, write, execute)
@@ -50,29 +51,59 @@
             fclose($firstNameFile); //closes the file for the OS.
             
             $firstNames = str_replace(array("\n", "\r"), '', $firstNames); //removes the new line characters from the string and casts it into an array
-
             $firstNamesArray = explode(",", $firstNames); //explode() removes the specified delimiter 
 
             print("<pre>");
             print("<h1>First Name Array</h1>");
             print_r($firstNamesArray);
+            //print_r(sizeof($firstNamesArray)); for debugging
             print("</pre>");
 
-            //TODO: instead of copying the above lines of code for each file, could write a function to automate it if there's time?
-            $lastNameFile =fopen("last_names.txt","r"); //arguments specify the file, the permissions (read, write, execute)
-            $lastNames = fgets($lastNameFile); //pulls the data line by line. NOTE: fgets() keeps the new line characters
-            fclose($lastNameFile); //closes the file for the OS.
-            
-            $lastNames = str_replace(array("\n", "\r"), '', $lastNames); //removes the new line characters from the string and casts it into an array
+//TODO: instead of copying the above lines of code for each file, could write a function to automate it if there's time?
+            //Last Names
+            $lastNamesFile =fopen("last_names.txt","r");
 
-            $lastNamesArray = explode(",", $lastNames); //explode() removes the specified delimiter 
+            while (!feof($lastNamesFile)) {
+                $lastNames = fgets($lastNamesFile);                                       //read a name (one line)
+                $lastNames = str_replace(array("\n", "\r"), '', $lastNames);              //remove newlines
+                //$lastNamesArray[] = $lastNames; //don't need. finalize in if statement to lose last newline
+                if (!feof($lastNamesFile)) {
+                    $lastNamesArray[] = $lastNames;
+                }
+            }
+            fclose($lastNamesFile);
 
             print("<pre>");
             print("<h1>Last Name Array</h1>");
             print_r($lastNamesArray);
             print("</pre>");
 
+
             //Street names
+            $streetNameFile =fopen("street_names.txt","r"); //arguments specify the file, the permissions (read, write, execute)
+            $streetNames = fgets($streetNameFile); //pulls the data line by line. NOTE: fgets() keeps the new line characters
+            fclose($streetNameFile); //closes the file for the OS.
+            $streetNamesString = ""; //declares an empty string. Needed to prevent throwing errors.
+
+            $streetNamesFile =fopen("street_names.txt","r");
+            while (!feof($streetNamesFile)) {
+                $streetNames = fgets($streetNamesFile);
+                if(!feof($streetNamesFile)) { //without this for loop, it will read and record an extra new line
+                    $streetNamesString = $streetNamesString . ":" . $streetNames;//finalize the array. it does add a : at the begining, which is a problem...
+                }
+            }
+            $streetNamesString = str_replace(array("\n", "\r"), '', $streetNamesString);//replace all new line characters and return line characters with blank spaces
+            $streetNamesArray = explode(":", $streetNamesString);//at each : delimiter, starts a new index in an array. WILL  PUT ONE AT INDEX 0
+//NOTE:when finalizing the string in line 102, due to the concatenation it was adding a : at the begining of the string in what would become index 0 on the array.
+//I found the link below on removing a particular index from an array and shifting the array to compensate for the removal
+//https://stackoverflow.com/questions/369602/deleting-an-element-from-an-array-in-php#:~:text=Deleting%20an%20element%20from%20an%20array%20in%20PHP,practice%20that%20has%20yet%20to%20be%20mentioned.%20
+            \array_splice($streetNamesArray,0,1);//removes index 0 and starts the array at index 1 (now new 0).
+
+            print("<pre>");
+            print("<h1>Street Names</h1>");
+            //print_r($streetNamesString); //for debugging
+            print_r($streetNamesArray);
+            print("</pre>");
 
 
             //Street Types
@@ -88,6 +119,24 @@
             print("<h1>Street Types</h1>");
             print_r($streetTypesArray);
             print("</pre>");
+
+//TODO:generate an html table populated with 25 unique names, addresses, and email addresses. 
+            $fullNames[] = array();//names array with unique first and last name 
+            for ($i=0; $i < 25; $i++) { 
+                $firstNameGenerator = rand(0, sizeof($firstNamesArray)-1);//generates a random value for the first name. neecds to be size -1 to include the final indexes.
+                $lastNameGenerator = rand(0, sizeof($lastNamesArray)-1);//generates a random value for the last name
+                $newName = "$firstNamesArray[$firstNameGenerator]" . " " . "$lastNamesArray[$lastNameGenerator]";
+                if (in_array($newName,$fullNames)) {//tests to see if the new name is already in the array
+//TODO:try again. can this be done without making a function?
+                }
+                $fullNames[$i] = $newName;
+            }
+            print("<pre>");//for debugging
+            print_r($fullNames);//for debugging
+            print("</pre>");//for debugging
+            $streetAddress = array();//address array with random generated numbers and street address
+            $houseNumber = rand(0, 999);//generates a random number between 0 and 999 inclusively
+            //email first name + . + last name + domain
 
         ?>
     </body>
