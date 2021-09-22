a1e0093e6fa9796efc4b1bd499c3b8d6c1cef275
Derek <55250148+DLB832@users.noreply.github.com>
2021-09-22 17:07:36 -0400

    Add files via upload
    TODO: finish test1

---
+++ Assignments/hw5/updateInfo.php
@@ -0,0 +1,5 @@
+<!Doctype html>
+<!--Author: Derek Campbell
+    Date: September 22, 2021
+    File: updateInfo.html
+    Purpose: TODO: checks the data submitted by movieInfo.html for proper formatting>
